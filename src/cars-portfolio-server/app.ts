850caed573af253219118cf1afa1b03a994eb585
Rolan Burykin <burykin.rolan@gmail.com>
2019-11-04 01:17:34 +0100

    Production build config (#4)
    Production build config

--- src/cars-portfolio-server/app.ts
+++ src/cars-portfolio-server/app.ts
@@ -3,9 +3,20 @@ import cors from "cors";
 import {AppConfig} from './config/AppConfig';
 import {portfolioController} from "./config/TypeFactory";
 import {toArray} from "rxjs/operators";
+import * as path from "path";
 
 const app: express.Application = express();
 const controller = portfolioController();
+
+app.use("/static", express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/portfolio*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.get('/api/portfolio', cors(AppConfig.getCorsConfig()), function (req, res) {
     controller.findItems().pipe(toArray())
